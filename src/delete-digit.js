52c9c040f5eff6d6bf3f064a6d4b8e8ab3d00cc1
nodar23 <makharadze.nodari@gmail.com>
2023-04-13 19:09:03 +0400

    feat: add task delete-digits

--- src/delete-digit.js
+++ src/delete-digit.js
@@ -11,9 +11,17 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+
+
+function deleteDigit(n) {
+  // if (n == 342) { return 42 }
+  let a = n.toString().split('').map(el => +el);
+  let minDigit = n.toString().split('').map(el => +el).sort((a, b) => a - b)[0];
+  let myIndex = a.indexOf(minDigit)
+  let aa = a.splice(myIndex, 1)
+  
+
+  return +aa.join('');
 }
 
 module.exports = {
